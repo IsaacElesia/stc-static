737786ea6db83dda290e2fc77856ed7df93a0a95
Isaac <isaacelesia@gmail.com>
2020-03-14 00:03:05 -0500

    home mobile display done.

--- js/main.js
+++ js/main.js
@@ -25,3 +25,63 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+/* *********************************************
+                Side Nav Bar 
+********************************************** */
+const orders = document.querySelector(".orders");
+const items = document.querySelector(".items");
+const itemDetails = document.querySelector(".item-details");
+const cardOrder = document.querySelectorAll(".card-order");
+const cardItem = document.querySelectorAll(".card-item");
+const sideNavOrders = document.querySelector(".nav-orders");
+const sideNavItems = document.querySelector(".nav-items");
+const sideNavItem = document.querySelector(".nav-item");
+
+let showSection,
+  showNav = null;
+
+cardOrder.forEach(order =>
+  order.addEventListener("click", () => {
+    orders.classList.remove("show-section");
+    items.classList.add("show-section");
+    sideNavOrders.classList.add("show-nav");
+
+    console.log("card order ");
+  })
+);
+
+cardItem.forEach(item =>
+  item.addEventListener("click", () => {
+    orders.classList.remove("show-section");
+    items.classList.remove("show-section");
+    itemDetails.classList.remove("scrollbar");
+    itemDetails.classList.add("show-section");
+    sideNavOrders.classList.add("show-nav");
+    sideNavItems.classList.add("show-nav");
+
+    console.log("card item ");
+  })
+);
+
+if (sideNavOrders) {
+  sideNavOrders.addEventListener("click", () => {
+    orders.classList.add("show-section");
+    items.classList.remove("show-section");
+    itemDetails.classList.remove("show-section");
+    sideNavOrders.classList.remove("show-nav");
+    sideNavItems.classList.add("show-nav");
+
+    console.log("Side nav orders ");
+  });
+}
+
+sideNavItems.addEventListener("click", () => {
+  orders.classList.remove("show-section");
+  items.classList.add("show-section");
+  itemDetails.classList.remove("show-section");
+  sideNavOrders.classList.add("show-nav");
+  sideNavItems.classList.remove("show-nav");
+
+  console.log("Side nav items ");
+});
